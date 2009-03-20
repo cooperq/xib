a74ff84062f6993a6d07736a21b733d4c8904940
Cooper Quintin <cooperq@cooperq.com>
2009-03-20 16:57:50 -0700

    update to helpers and index for projects and sessions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,5 @@
 module SessionsHelper
-  def session_list(project=false)
+  def session_list(project)
     @sessions = project ? Session.find(:all, :options => {:project_id => project.id}) : Session.find(:all)
   end
 end
