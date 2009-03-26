60286018f24ac89f659df77f03cf33b0b5ec519a
Cooper Quintin <cooperq@cooperq.com>
2009-03-25 22:45:54 -0700

    times are now displayed as hours:minutes

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,9 @@ module SessionsHelper
   def session_list(project=false)
     @sessions = project ? Session.find_by_project_id(project).to_a : Session.find(:all)
   end
+
+  def minutes_to_time(minutes)
+    (minutes / 60).to_s + ":" + "%02d" % (minutes % 60)
+  end
 end
+
